179c55d1715861457f22f039630155ceeba670d8
512372507@qq.com <512372507@qq.com>
2016-10-18 17:02:53 +0800

    修改返回

--- test/src/util/StringIfInSelf.java
+++ test/src/util/StringIfInSelf.java
@@ -8,18 +8,18 @@ public class StringIfInSelf {
 	/**
 	 * 判断字符串里面是否有相同的字符
 	 * @param tmp
-	 * @return
+	 * @return 都不相同返回true
 	 */
 	public static boolean isExist(String tmp) {
-		boolean isOk = false;
+		boolean isOk = true;
 		char[] tmpchar = tmp.toCharArray();
 		for (int i = 0; i < tmpchar.length; i++) {
 			for (int j = i + 1; j < tmpchar.length; j++) {
 				if (tmpchar[i] == tmpchar[j]) {
-					isOk = true;
+					isOk = false;
 					return isOk;
 				} else {
-					isOk = false;
+					isOk = true;
 				}
 			}
 		}
